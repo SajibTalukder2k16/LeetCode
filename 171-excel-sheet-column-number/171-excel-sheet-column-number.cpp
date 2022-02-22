1af8578dec98fce6a8c91a819f2ca9617e14adcb
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-02-23 00:54:39 +0600

    Time: 7 ms (16.06%), Space: 5.9 MB (92.06%) - LeetHub

--- 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
+++ 171-excel-sheet-column-number/171-excel-sheet-column-number.cpp
@@ -5,16 +5,14 @@ public:
         long long prefixSum[8]={0};
         long long mulVal[8];
         mulVal[0]=1;
-        for(int i=1;i<=len;i++)
+        long long ret=0;prefixSum[len-1];
+        for(int i=1,j=len-1;j>=0;j--,i++)
         {
             mulVal[i]=mulVal[i-1]*26;
             prefixSum[i]+=prefixSum[i-1]+mulVal[i];
+            ret+=(columnTitle[j]-'A')*mulVal[i-1];
         }
-        long long ret=prefixSum[len-1];
-        for(int i=0,j=len-1;j>=0;j--,i++)
-        {
-            ret+=(columnTitle[j]-'A')*mulVal[i];
-        }
+        ret+=prefixSum[len-1];
         ret++;
         return ret;
         
