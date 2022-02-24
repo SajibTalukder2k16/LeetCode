50d7d9ffeb333d9f221407f804958044c48a62ba
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-02-25 01:10:54 +0600

    Time: 26 ms (10.45%), Space: 11.6 MB (36.30%) - LeetHub

--- 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
+++ 83-remove-duplicates-from-sorted-list/83-remove-duplicates-from-sorted-list.cpp
@@ -11,7 +11,7 @@
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(head==NULL)
+        if(head==NULL||head->next==NULL)
             return head;
 
         ListNode* ret=head;
