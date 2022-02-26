4f577970132508b2482a459ad38661e7fd5a4cae
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-02-27 00:59:23 +0600

    Time: 184 ms (60.00%), Space: 22.4 MB (98.04%) - LeetHub

---
+++ 646-maximum-length-of-pair-chain/646-maximum-length-of-pair-chain.cpp
@@ -0,0 +1,32 @@
+#define mx 2501
+class Solution {
+public:
+    int dp[mx];
+    
+    int foo(int i,vector<vector<int>>& nums,int sz){
+        if(dp[i]!=-1)
+            return dp[i];
+        int ans=0;
+        for(int j=i+1;j<sz;j++)
+        {
+            if(nums[j][0]>nums[i][1]){
+                if(dp[j]!=-1)
+                    ans=max(ans,dp[j]);
+                else
+                    ans=max(ans,foo(j,nums,sz));
+            }
+        }
+        return dp[i]=ans+1;
+    }
+    int findLongestChain(vector<vector<int>>& pairs) {
+        memset(dp,-1,sizeof dp);
+        int ans=1;
+        int sz=pairs.size();
+        sort(pairs.begin(),pairs.end());
+        for(int i=0;i<sz;i++)
+        {
+            ans=max(ans,foo(i,pairs,sz));
+        }
+        return ans;
+    }
+};
