9bea3d042557cd7001b149a0167263656644b14c
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-02-27 15:14:26 +0600

    Time: 51 ms (46.46%), Space: 25.8 MB (94.92%) - LeetHub

--- 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
+++ 977-squares-of-a-sorted-array/977-squares-of-a-sorted-array.cpp
@@ -2,10 +2,60 @@ class Solution {
 public:
     vector<int> sortedSquares(vector<int>& nums) {
         int sz=nums.size();
-        for(int i=0;i<sz;i++)
+        int mn=100000;
+        int mnIndex=-1;
+        for(int i=0;i<sz;i++){
+            if(nums[i]>=0 && mn>nums[i]){
+                mn=nums[i];
+                mnIndex=i;
+            }
             nums[i]*=nums[i];
-        sort(nums.begin(),nums.end());
-        return nums;
+        }
+        if(mnIndex==-1)
+        {
+            int half=sz/2;
+            for(int i=0;i<half;i++){
+                swap(nums[i],nums[sz-1]);
+                sz--;
+            }
+            return nums;
+                
+                
+        }
+        //cout<<"Hello"<<endl;
+        vector<int>vec(sz);
+        int idx=0;
+        //int val=nums[mnIndex];
+        //vec[idx++]=val;
+        //cout<<"Hello"<<endl;
+        int i,j;
+        i=mnIndex;
+        j=mnIndex-1;
+        //cout<<"Hello"<<endl;
+        for(;i<sz && j>=0;){
+            if(nums[i]<=nums[j])
+            {
+                vec[idx++]=nums[i];
+                i++;
+            }
+            else{
+                vec[idx++]=nums[j];
+                j--;
+            }
+        }
+        if(i<sz)
+        {
+            for(;i<sz;i++)
+                vec[idx++]=nums[i];
+        }
+        if(j>=0){
+            for(;j>=0;j--)
+                vec[idx++]=nums[j];
+        }
+        //nums=vec;
+            
+        //sort(nums.begin(),nums.end());
+        return vec;
         
     }
 };
