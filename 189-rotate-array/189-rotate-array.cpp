53b3b3c9cda0ad589399a347d3865b495f6582c6
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-02-27 17:32:34 +0600

    Time: 32 ms (79.00%), Space: 25 MB (49.68%) - LeetHub

--- 189-rotate-array/189-rotate-array.cpp
+++ 189-rotate-array/189-rotate-array.cpp
@@ -1,17 +1,34 @@
 class Solution {
+    void foo(int index,int k,int sz,vector<int> & nums){
+        int temp=nums[index];
+        int idx=index;
+        int val;
+        index+=k;
+        while(index!=idx){
+            val=nums[index];
+            nums[index]=temp;
+            temp=val;
+            if(index+k<sz)
+                index+=k;
+            else{
+                index+=k;
+                index=index-sz;
+            }
+        }
+        nums[idx]=temp;
+    }
 public:
     void rotate(vector<int>& nums, int k) {
         if(k==0)
             return;
         int sz=nums.size();
         k=k%sz;
-        vector<int>ara(sz);
-        int i,j;
-        for(i=0,j=k;j<sz;i++,j++)
-            ara[j]=nums[i];
-        for(j=0;j<k;j++)
-            ara[j]=nums[i++];
-        nums=ara;
-        
+        if(k==0)
+            return ;
+        int gcd = __gcd(k,sz);
+        for(int i=0;i<gcd;i++)
+            foo(i,k,sz,nums);
+
+            
     }
 };
