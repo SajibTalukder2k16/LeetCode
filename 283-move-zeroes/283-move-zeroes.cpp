5bfcb8e1e0dafbefc6417919ab00f0c3903cbcb0
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-02-28 22:07:52 +0600

    Time: 24 ms (81.24%), Space: 19.4 MB (13.91%) - LeetHub

--- 283-move-zeroes/283-move-zeroes.cpp
+++ 283-move-zeroes/283-move-zeroes.cpp
@@ -2,13 +2,13 @@ class Solution {
 public:
     void moveZeroes(vector<int>& nums) {
         int sz=nums.size();
-        int index=0;
-        for(int i=0;i<sz;i++){
+        int index=0,i;
+        for(i=0;i<sz;i++){
             if(nums[i]!=0){
                 nums[index++]=nums[i];
             }
         }
-        for(int i=index;i<sz;i++)
+        for(i=index;i<sz;i++)
             nums[i]=0;
         
     }
