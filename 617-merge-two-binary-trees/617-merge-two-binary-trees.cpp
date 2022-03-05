5ea6278d9ba314ff367189ececc81dbce95e9a3c
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-03-06 00:29:08 +0600

    Time: 96 ms (5.21%), Space: 33 MB (17.19%) - LeetHub

--- 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -11,45 +11,32 @@
  */
 class Solution {
 public:
-    void traversing(TreeNode* root1, TreeNode* root2,TreeNode *root){
+    TreeNode* traversing(TreeNode* root1, TreeNode* root2,TreeNode *root){
         if(root1==NULL && root2==NULL)
         {
             root=NULL;
-            return;
+            return root;
         }
+        root=new TreeNode;
         if(root1==NULL){
             root->val=root2->val;
             root->left=root2->left;
             root->right=root2->right;
-            return;
+            return root;
         }
         if(root2==NULL){
             root->val=root1->val;
             root->left=root1->left;
             root->right=root1->right;
-            return;
+            return root;
         }
-        TreeNode* leftNode=new TreeNode();
-        TreeNode* rightNode=new TreeNode();
-        root->left=leftNode;
-        root->right=rightNode;
         root->val=root1->val+root2->val;
-        if(root1->left!=NULL || root2->left!=NULL)
-            traversing(root1->left,root2->left,leftNode);
-        else
-            root->left=NULL;
-        if(root1->right!=NULL || root2->right!=NULL)
-            traversing(root1->right,root2->right,rightNode);
-        else
-            root->right=NULL;
-        return;
-            
+        root->left=traversing(root1->left,root2->left,root->left);
+        root->right=traversing(root1->right,root2->right,root->right);
+        return root;   
     }
     TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
-        if(root1==NULL && root2==NULL)
-            return root1;
-        TreeNode* root=new TreeNode();
-        traversing(root1,root2,root);
-        return root;
+        TreeNode* root=NULL;
+        return traversing(root1,root2,root);
     }
 };
