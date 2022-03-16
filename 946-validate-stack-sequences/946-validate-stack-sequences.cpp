b7e7ec685362606db8226e52f0aeeda5a4834730
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-03-17 00:21:07 +0600

    Time: 19 ms (22.52%), Space: 15.4 MB (59.16%) - LeetHub

--- 946-validate-stack-sequences/946-validate-stack-sequences.cpp
+++ 946-validate-stack-sequences/946-validate-stack-sequences.cpp
@@ -9,28 +9,21 @@ public:
         int i=0,j=0;
         int top=-1;
         while(true){
-            
             if(j==lenPopped)
                 break;
             if(!push_stack.empty())
                 top=push_stack.top();
             if(top==popped[j]){
-                //cout<<"Here: "<<top<<endl;
                 while(top==popped[j]){
-                    //[4,5,3,2,1]
-                    //cout<<top<<" before "<<popped[j]<<endl;
                     push_stack.pop();
                     j++;
                     if(!push_stack.empty())
                         top=push_stack.top();
                     else
                         break;
-                    
-                    //cout<<top<<" after "<<popped[j]<<endl;
                     if(j==lenPopped)
                         break;
                 }
-                //cout<<j<<endl;
                 if(j==lenPopped)
                     break;
             }
@@ -42,7 +35,6 @@ public:
                 
             }
         }
-        //cout<<"Hello"<<endl;
         if(j==lenPopped)
             return true;
         return false;
