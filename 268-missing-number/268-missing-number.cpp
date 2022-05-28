8174ef567324cd7c8809cfc9f1779a278b7e904f
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-05-28 23:08:59 +0600

    Time: 18 ms (87.39%), Space: 18.1 MB (20.98%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -1,12 +1,14 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        int totalSum=0;
         int sum=0;
         int sz=nums.size();
         for(int i=0;i<sz;i++){
             sum+=nums[i];
+            totalSum+=i+1;
         }
-        return (sz*(sz+1))/2-sum;
+        return totalSum-sum;
         
     }
 };
