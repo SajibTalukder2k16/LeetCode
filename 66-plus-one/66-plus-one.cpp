f7109e84c941e65b1503461c7d0c064df9af48e8
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-07-22 13:01:03 +0600

    Time: 4 ms (43.62%), Space: 8.6 MB (85.58%) - LeetHub

--- 66-plus-one/66-plus-one.cpp
+++ 66-plus-one/66-plus-one.cpp
@@ -1,25 +1,26 @@
 class Solution {
 public:
     vector<int> plusOne(vector<int>& digits) {
-        int added[] = {1,2,3,4,5,6,7,8,9,0};
+        int added[] = {1,2,3,4,5,6,7,8,9};
         int sz = digits.size();
         
         if(digits[sz-1]!=9)
             digits[sz-1]=added[digits[sz-1]];
         else{
             while(digits[sz-1]==9){
-                digits[sz-1]=added[digits[sz-1]];
+                digits[sz-1]=0;
                 sz-=1;
                 if(sz==0)
                     break;
             }
-            if(sz==0){
-                vector<int>first;
-                first.push_back(1);
-                first.insert(first.end(),digits.begin(),digits.end());
-                return first;
+            if(sz==0)
+            {
+                digits[0]=1;
+                digits.push_back(0);
             }
-            digits[sz-1]=added[digits[sz-1]];
+            else
+                digits[sz-1]=added[digits[sz-1]];
+            
         }
         return digits;
         
