881013e75b995e5073258427220a0489f475a8a8
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-08-07 23:48:38 +0600

    Time: 8 ms (56.32%), Space: 7.8 MB (50.99%) - LeetHub

--- 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
+++ 1672-richest-customer-wealth/1672-richest-customer-wealth.cpp
@@ -3,14 +3,15 @@ public:
     int maximumWealth(vector<vector<int>>& accounts) {
         int maxWealth = 0;
         int sz = accounts.size();
+        int noOfbanks = accounts[0].size();
+        int sum=0;
         for(int i=0;i<sz;i++){
-            int noOfbanks = accounts[i].size();
-            int sum=0;
             for(int j=0;j<noOfbanks;j++){
                 sum+=accounts[i][j];
             }
             if(sum>maxWealth)
                 maxWealth=sum;
+            sum=0;
         }
         return maxWealth;
     }
