ac0e95f2520d04e2d0eed37eb67a388376ddcce4
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-09-06 16:09:53 +0600

    Time: 4 ms (49.16%), Space: 9.6 MB (97.40%) - LeetHub

--- 226-invert-binary-tree/226-invert-binary-tree.cpp
+++ 226-invert-binary-tree/226-invert-binary-tree.cpp
@@ -15,9 +15,7 @@ public:
         if(root!=NULL){
             invertTree(root->left);
             invertTree(root->right);
-            TreeNode* temp = root->left;
-            root->left=root->right;
-            root->right = temp;
+            swap(root->left,root->right);
         }
         return root;
     }
