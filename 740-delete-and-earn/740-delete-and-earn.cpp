f879fdb8cef2189653c05224a241a41a7192dc9c
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-09-17 17:49:53 +0600

    Time: 45 ms (15.43%), Space: 14.5 MB (34.48%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -3,34 +3,36 @@ public:
     int dp[10001]={0};
     int sz = 0;
     map<int,int>mp;
-    int foo(vector<int>& uniques, int i){
-        // cout<<i<<endl;
-        // cout<<dp[i]<<" "<<sz<<endl;
+    vector<int>uniques;
+    int foo(int i){
         if(i>=sz)
             return 0;
         if(dp[i]!=-1)
             return dp[i];
-        if(mp[uniques[i]+1]!=0)
-            dp[i]=max(uniques[i]*mp[uniques[i]]+foo(uniques,i+2),foo(uniques,i+1));
+        int val = uniques[i];
+        int earn = val*mp[val];
+        if(i+1==sz)
+            return earn;
+        if(uniques[i+1]==val+1)
+            dp[i]=max(earn + foo(i+2),foo(i+1));
         else
-            dp[i]=max(uniques[i]*mp[uniques[i]]+foo(uniques,i+1),foo(uniques,i+1));
+            dp[i]=max(earn+foo(i+1),foo(i+1));
         return dp[i];
     }
     int deleteAndEarn(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        vector<int>uniques;
         sz = nums.size();
-        int cnt=0;
+        int cnt = 0;
         for(int i=0;i<sz;i++){
-            // cout<<mp[nums[i]]<<nums[i]<<endl;
             if(mp[nums[i]]==0){
                 uniques.push_back(nums[i]);
                 cnt++;
-            }  
+            }
+                
             mp[nums[i]]++; 
         }
         sz = cnt;
         memset(dp,-1,sizeof(dp));
-        return foo(uniques,0);
+        return foo(0);
     }
 };
