88cdcf4127a29c97c37e49c2293b51d0b96d2df8
Sajib Talukder <30448179+SajibTalukder2k16@users.noreply.github.com>
2022-09-17 17:46:50 +0600

    Time: 38 ms (22.39%), Space: 14.5 MB (34.03%) - LeetHub

--- 740-delete-and-earn/740-delete-and-earn.cpp
+++ 740-delete-and-earn/740-delete-and-earn.cpp
@@ -1,34 +1,44 @@
+#define minusInf -300000000
+#define mx 20001
 class Solution {
 public:
-    int dp[10001]={0};
-    int sz = 0;
-    map<int,int>mp;
-    vector<int>uniques;
-    int foo(int i){
-        if(i>=sz)
+    int sz;
+    int dp[mx];
+    vector<int>uniqueNums;
+    map<int,int>mapCount;
+    int foo(int pos){
+        if(pos>=sz)
             return 0;
-        if(dp[i]!=-1)
-            return dp[i];
-        int val = uniques[i];
-        int earn = val*mp[val];
-        if(i+1==sz)
-            return earn;
-        if(uniques[i+1]==val+1)
-            dp[i]=max(earn + foo(i+2),foo(i+1));
-        else
-            dp[i]=max(earn+foo(i+1),foo(i+1));
-        return dp[i];
+        if(dp[pos]!=-1)
+            return dp[pos];
+        int val=uniqueNums[pos];
+        int nextPos=pos+1;
+        int res1=val*mapCount[val];
+        if(nextPos==sz)
+            return res1;
+        if(uniqueNums[nextPos]==val+1)
+            nextPos++;
+        res1+=foo(nextPos);
+        int res2=foo(pos+1);
+        dp[pos]=max(res1,res2);
+        return dp[pos];
+        
     }
     int deleteAndEarn(vector<int>& nums) {
         sort(nums.begin(),nums.end());
-        sz = nums.size();
+        memset(dp,-1,sizeof dp);
+        sz=nums.size();
+        int cnt=0;
         for(int i=0;i<sz;i++){
-            if(mp[nums[i]]==0)
-                uniques.push_back(nums[i]);
-            mp[nums[i]]++; 
+            if(mapCount[nums[i]]==0){
+                uniqueNums.push_back(nums[i]);
+                cnt++;
+            }   
+            mapCount[nums[i]]++;
         }
-        sz = uniques.size();
-        memset(dp,-1,sizeof(dp));
-        return foo(0);
+        sz=cnt;
+        int res=foo(0);
+
+        return res;
     }
 };
